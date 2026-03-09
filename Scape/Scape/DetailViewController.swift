c4547dad3986947a144185b89bf5bb68d2f205ea
kmartinez899 <mkristian8992@gmail.com>
2026-03-09 14:53:08 -0500

    added depaul buildings to table view database

--- Scape/Scape/DetailViewController.swift
+++ Scape/Scape/DetailViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var buildingNameLabel: UILabel!
+    
+    @IBOutlet weak var buildingDescLabel: UILabel!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    var building: Building?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let b = building {
+            buildingNameLabel.text = b.name
+            buildingDescLabel.text = b.description + "\n" + "Address: " + b.address
+            image.image = UIImage(named: b.imageName)
+        }
     }
     
 
--- Scape/Scape/scapeBuildings.swift
+++ Scape/Scape/scapeBuildings.swift
@@ -8,55 +8,79 @@
 import Foundation
 
 let buildings = [
+    // Loop Campus
     Building(name: "DePaul Center",
         address: "1 E Jackson Blvd",
+        campus: "Loop Campus",
         description: "Home of the School of Business. Has 11 floors.",
         imageName: "depaulcenter"),
     
     Building(name: "CDM Center",
              address: "243 S Wabash Ave",
+             campus: "Loop Campus",
              description: "Home of the School of Computing and Digital Media.",
              imageName: "cdm"),
     
     Building(name: "Lewis Center",
-                 address: "25 E Jackson Blvd",
-                 description: "Home of the DePaul College of Law.",
-                 imageName: "lewiscenter"),
+             address: "25 E Jackson Blvd",
+             campus: "Loop Campus",
+             description: "Home of the DePaul College of Law.",
+             imageName: "lewiscenter"),
     
     Building(name: "Daley Building",
-                 address: "14 E Jackson St",
-                 description: "Loop academic building used for classrooms and faculty offices.",
-                 imageName: "daley"),
+             address: "14 E Jackson St",
+             campus: "Loop Campus",
+             description: "Loop academic building used for classrooms and faculty offices.",
+             imageName: "daley"),
     
+    // Lincoln Park Campus
     Building(name: "Schmitt Academic Center",
-                 address: "2320 N Kenmore Ave",
-                 description: "Major academic building with classrooms, labs, and study spaces.",
-                 imageName: "sac"),
+             address: "2320 N Kenmore Ave",
+             campus: "Lincoln Park Campus",
+             description: "Major academic building with classrooms, labs, and study spaces.",
+             imageName: "sac"),
 
     Building(name: "John T. Richardson Library",
-                 address: "2350 N Kenmore Ave",
-                 description: "Main Lincoln Park campus library with study areas and research resources.",
-                 imageName: "richardson"),
+             address: "2350 N Kenmore Ave",
+             campus: "Lincoln Park Campus",
+             description: "Main Lincoln Park campus library with study areas and research resources.",
+             imageName: "richardson"),
 
     Building(name: "Student Center",
-                 address: "2250 N Sheffield Ave",
-                 description: "Hub for student life with dining, meeting rooms, and organization offices.",
-                 imageName: "studentcenter"),
+             address: "2250 N Sheffield Ave",
+             campus: "Lincoln Park Campus",
+             description: "Hub for student life with dining, meeting rooms, and organization offices.",
+             imageName: "studentcenter"),
 
     Building(name: "Ray Meyer Fitness and Recreation Center",
-                 address: "2235 N Sheffield Ave",
-                 description: "Campus recreation center with gym, fitness classes, and basketball arena.",
-                 imageName: "ray"),
+             address: "2235 N Sheffield Ave",
+             campus: "Lincoln Park Campus",
+             description: "Campus recreation center with gym, fitness classes, and basketball arena.",
+             imageName: "ray"),
     
     Building(name: "Levan Center",
-                 address: "2322 N Kenmore Ave",
-                 description: "Administrative offices and student services building.",
-                 imageName: "levan"),
+             address: "2322 N Kenmore Ave",
+             campus: "Lincoln Park Campus",
+             description: "Administrative offices and student services building.",
+             imageName: "levan"),
     
     Building(name: "School of Music",
-                 address: "804 W Belden Ave",
-                 description: "Home of the School of Music with concert halls and rehearsal spaces.",
-                 imageName: "music"),
+             address: "804 W Belden Ave",
+             campus: "Lincoln Park Campus",
+             description: "Home of the School of Music with concert halls and rehearsal spaces.",
+             imageName: "music"),
+    
+    Building(name: "Arts & Letters Hall",
+         address: "2311 N Kenmore Ave",
+         campus: "Lincoln Park Campus",
+         description: "Home to many humanities departments including history, philosophy, and language studies. Contains classrooms, faculty offices, and student study spaces.",
+         imageName: "artsandletters"),
+    
+    Building(name: "The Theatre School",
+         address: "2350 N Racine Ave",
+         campus: "Lincoln Park Campus",
+         description: "Home of DePaul’s nationally recognized Theatre School. The building contains performance spaces, rehearsal rooms, classrooms, and production studios for students studying acting, directing, design, and theatre technology.",
+         imageName: "theatre"),
 
 
     ]
@@ -65,12 +89,14 @@ class Building {
     
     var name: String
     var address: String
+    var campus: String
     var description: String
     var imageName: String
     
-    init(name: String, address: String, description: String, imageName: String) {
+    init(name: String, address: String, campus: String, description: String, imageName: String) {
         self.name = name
         self.address = address
+        self.campus = campus
         self.description = description
         self.imageName = imageName
     }
